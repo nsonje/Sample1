faca4977917ae7cd97de286eee660ef4f04b6ab6
Ninad Sonje <ninad.sonje@gmail.com>
2013-02-14 11:44:09 +0530

    Initial commit

---
+++ Sample1/Data.java
@@ -0,0 +1,18 @@
+
+public class Data {
+	private int num;
+	
+	/*
+	 * Set the value of the num.
+	 */
+	public void setData(int n) {
+		num = n;
+	}
+	
+	/*
+	 * Get the value of the num. 
+	 */
+	public int getData() {
+		return num;
+	}
+}
---
+++ Sample1/Demo.java
@@ -0,0 +1,15 @@
+
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Data d = new Data();
+		
+		d.setData(10);
+		System.out.println("The data is: " + d.getData());
+	}
+
+}
